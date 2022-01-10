233d79febade41f7fa10dba018d020cb3a935d4e
Bimi <75537408+Bimi05@users.noreply.github.com>
2022-01-10 14:04:09 +0200

    Bump version to v1.0.3

--- bstats/__init__.py
+++ bstats/__init__.py
@@ -11,7 +11,8 @@ Copyright (c) 2022-present Bimi05
 __title__ = "bstats"
 __author__ = "Bimi05"
 __license__ = "MIT"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from .clients import SyncClient, AsyncClient
 from .models import *
+from .errors import *
