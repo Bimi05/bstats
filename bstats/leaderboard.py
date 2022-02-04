0e15bcc264ed0ebe834b3f064e4481e357ff42d3
Bimi <dim.ziakas07@gmail.com>
2022-02-04 17:53:49 +0200

    update: docstrings

--- bstats/leaderboard.py
+++ bstats/leaderboard.py
@@ -29,38 +29,42 @@ from .utils import camel_to_snake
 class LeaderboardEntry:
     """
     Represents a Brawl Stars leaderboard ranking entry.
-    
-    Attributes for Players/Brawlers rankings
-    ----------
 
-    name: ``str``
+    ### Attributes
+    name: `str`
         The player's or the club's name.
-    tag: ``str``
-        The player's or the club's unique tag.
-    trophies: ``int``
+    tag: `str`
+        The player's or the club's tag.
+    trophies: `int`
         The player's or the club's current total trophies.
-    rank: ``int``
+    rank: `int`
         The player's or the club's leaderboard rank.
-    color: Optional[``str``]
-        The hex code representing the player's name colour. ``None`` if the current leaderboard rankings are focused on clubs.
-    colour: Optional[``str``]
-        An alias to ``LeaderboardPlayerEntry.color``. ``None`` if the current leaderboard rankings are focused on clubs.
-    icon_id: Optional[``int``]
-        The ID of the player's icon. ``None`` if the current leaderboard rankings are focused on clubs.
-    club_name: Optional[``str``]
-        The player's club's name, ``None`` if the current leaderboard rankings are focused on clubs or if the member is not in any club.
-    member_count: Optional[``int``]
-        The club's current amount of members. ``None`` if the current leaderboard rankings are focused on players.
-    badge_id: Optional[``int``]
-        The club's badge ID. ``None`` if the current leaderboard rankings are focused on players.
+    color: Optional[`str`]
+        The hex code representing the player's name colour.
+        `None` if the current leaderboard rankings are focused on clubs.
+        An alias exists, `.colour`.
+    colour: Optional[`str`]
+        The hex code representing the player's name color.
+        `None` if the current leaderboard rankings are focused on clubs.
+        An alias exists, `.color`.
+    icon_id: Optional[`int`]
+        The ID of the player's icon.
+        `None` if the current leaderboard rankings are focused on clubs.
+    club_name: Optional[`str`]
+        The player's club's name.
+        `None` if the member is not in any club or the current leaderboard rankings are focused on clubs.
+    member_count: Optional[`int`]
+        The club's current amount of members.
+        `None` if the current leaderboard rankings are focused on players.
+    badge_id: Optional[`int`]
+        The club's badge ID.
+        `None` if the current leaderboard rankings are focused on players.
     """
     def __init__(self, ranking: dict) -> None:
-        self.ranking = {}
-        for key in ranking:
-            self.ranking[camel_to_snake(key)] = ranking[key]
+        self.ranking = {camel_to_snake(key): value for key, value in ranking.items()}
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} object rank={self.ranking['rank']} name='{self.ranking['name']}' tag='{self.ranking['tag']}'>"
+        return f"<{self.__class__.__name__} rank={self.ranking['rank']} name={self.ranking['name']!r} tag={self.ranking['tag']!r}>"
 
     def __str__(self) -> str:
         return f"Rank {self.ranking['rank']}: {self.ranking['name']} ({self.ranking['tag']})"
@@ -68,66 +72,51 @@ class LeaderboardEntry:
 
     @property
     def name(self) -> str:
-        """``str``: The player's or the club's name."""
+        """`str`: The player's or the club's name."""
         return self.ranking["name"]
 
     @property
     def tag(self) -> str:
-        """``str``: The player's or the club's unique tag."""
+        """`str`: The player's or the club's unique tag."""
         return self.ranking["tag"]
 
     @property
     def trophies(self) -> int:
-        """``int``: The player's or the club's current total trophies."""
+        """`int`: The player's or the club's current total trophies."""
         return self.ranking["trophies"]
 
     @property
     def rank(self) -> int:
-        """``int``: The player's or the club's leaderboard rank."""
+        """`int`: The player's or the club's leaderboard rank."""
         return self.ranking["rank"]
 
     @property
     def color(self) -> Optional[str]:
-        """Optional[``str``]: The hex code representing the player's name colour. ``None`` if the current leaderboard rankings are focused on clubs."""
-        try:
-            return self.ranking["name_color"]
-        except KeyError:
-            return None
+        """Optional[`str`]: The hex code representing the player's name colour. `None` if the current leaderboard rankings are focused on clubs. An alias exists, `.colour`."""
+        return self.ranking.get("name_color")
 
     @property
     def colour(self) -> Optional[str]:
-        """An alias to ``LeaderboardPlayerEntry.color``."""
+        """Optional[`str`]: The hex code representing the player's name colour. `None` if the current leaderboard rankings are focused on clubs. An alias exists, `.colour`.."""
         return self.color
 
     @property
     def icon_id(self) -> Optional[int]:
-        """Optional[``int``]: The ID of the player's icon. ``None`` if the current leaderboard rankings are focused on clubs."""
-        try:
-            return self.ranking["icon"]["id"]
-        except KeyError:
-            return None
+        """Optional[`int`]: The ID of the player's icon. `None` if the current leaderboard rankings are focused on clubs."""
+        return self.ranking.get("icon").get("id")
 
     @property
     def club_name(self) -> Optional[str]:
-        """Optional[``str``]: The player's club's name. ``None`` if the current leaderboard rankings are focused on clubs or if the member is not in any club."""
-        try:
-            return self.ranking["club"]["name"]
-        except KeyError:
-            return None
+        """Optional[`str`]: The player's club's name. `None` if the member is not in any club or the current leaderboard rankings are focused on clubs."""
+        return self.ranking.get("club").get("name")
 
     @property
     def member_count(self) -> Optional[int]:
-        """Optional[``int``]: The club's current amount of members. ``None`` if the current leaderboard rankings are focused on players."""
-        try:
-            return self.ranking["member_count"]
-        except KeyError:
-            return None
+        """Optional[`int`]: The club's current amount of members. `None` if the current leaderboard rankings are focused on players."""
+        return self.ranking.get("member_count")
 
     @property
     def badge_id(self) -> Optional[int]:
-        """Optional[``int``]: The club's badge ID. ``None`` if the current leaderboard rankings are focused on players."""
-        try:
-            return self.ranking["badge_id"]
-        except KeyError:
-            return None
+        """Optional[`int`]: The club's badge ID. `None` if the current leaderboard rankings are focused on players."""
+        return self.ranking.get("badge_id")
 
